5b3bd28778ec9bd877734eb31576215c9f09504b
Brian <briomwaura@gmail.com>
2024-02-13 10:41:35 +0300

    assignment commit

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import ReactRouterDom, { Route } from "react-router-dom";
 import MoviesList from "./MoviesList";
 
 function MoviesPage({ movies }) {
   return (
     <div>
-      <MoviesList movies={movies} />
+      <ReactRouterDom>
+        <Route>
+        <MoviesList movies={movies} />
+        </Route>
+      </ReactRouterDom>
+      
     </div>
   );
 }
